2ae51ab9e7adce1170b5f2e99ae755432592951c
MarilynWaldman <mdwaldman22@gmail.com>
2021-11-16 13:04:13 -0700

    geocodeit example

--- geocodeit.py
+++ geocodeit.py
@@ -8,23 +8,51 @@ import geopandas as gpd
 import geopy
 from geopy.geocoders import Nominatim
 from geopy.extra.rate_limiter import RateLimiter
+from streamlit.logger import setup_formatter
 
 def create_address_col(df):
     st.sidebar.title("Select Address columns")
     st.sidebar.info("Select columns:  Street Address, City, State and Zip Code")
+
+    dflist = df.columns.tolist()
+    dflist.insert(0,"None")
+    print(dflist)
     
-    address_name = st.sidebar.selectbox("Select Address Column", df.columns.tolist())
-    city = st.sidebar.selectbox("Select City Column", df.columns.tolist())
-    state = st.sidebar.selectbox("Select the State Column", df.columns.tolist())
-    zip_code = st.sidebar.selectbox("Select the Zip Code Column", df.columns.tolist())
+    df["geocode_col"] = None
+    address_name = st.sidebar.selectbox("Select Address Column", dflist)
+    city = st.sidebar.selectbox("Select City Column", dflist)
+    state = st.sidebar.selectbox("Select the State Column", dflist)
+    zip_code = st.sidebar.selectbox("Select the Zip Code Column", dflist)
 
     
+    print(address_name)
+    print(type(address_name))
+    
+    if df['geocode_col'].isnull().values.any():
+        print("is empty")
+    
+    if address_name != "None":
+        df["geocode_col"] =  df[address_name].astype(str) 
+        
+    if city != "None":
+        if df['geocode_col'].isnull().values.any():
+            df["geocode_col"] =  df[city].astype(str) 
+        else:    
+            df["geocode_col"] = df["geocode_col"] + "," +  df[city].astype(str) 
 
-    df["geocode_col"] =  df[address_name].astype(str) + ',' + \
-                df[city].astype(str) + ',' + \
-                df[state].astype(str) + ',' + \
-                df[zip_code].astype(str) 
-       
+    if state != "None":
+        if df['geocode_col'].isnull().values.any():
+            df["geocode_col"] = df[state].astype(str) 
+        else:    
+            df["geocode_col"] = df["geocode_col"] +  "," +  df[state].astype(str) 
+
+    if zip_code != "None":
+        if df['geocode_col'].isnull().values.any():
+            df["geocode_col"] = df[zip_code].astype(int).astype(str)
+        else:    
+            df["geocode_col"] = df["geocode_col"] +  ',' + df[zip_code].astype(int).astype(str) 
+                         
+
     return df
     
 def choose_geocode_column(df):
